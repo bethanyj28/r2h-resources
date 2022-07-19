2de8f5d01874eef72f1a5634a42894c8005468f4
Bethany <bethanyj28@users.noreply.github.com>
2022-07-18 20:23:52 -0400

    Switch + and -

--- react_state/src/components/function_counter.js
+++ react_state/src/components/function_counter.js
@@ -5,8 +5,8 @@ function FunctionCounter() {
         <h1>I'm a function component</h1>
       <h1>{number}</h1>
       <div className="buttonContainer">
-        <button>+</button>
         <button>-</button>
+        <button>+</button>
       </div>
     </div>
   )
